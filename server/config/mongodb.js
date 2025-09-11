1e9c15987aa2abaa1fb35cdc141bc0719dbc2568
GauriNagariya <gaurinagariya@gmail.com>
2025-09-11 22:31:33 +0530

    modified mongodb url in env and mongodb.js file

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,11 +1,29 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async ()=>{
+
+//     mongoose.connection.on('connected', ()=>{
+//         console.log("Database Connected")
+//     })
+//     await mongoose.connect(`${process.env.MONGODB_URI}/lumeo`)
+// }
+
+// export default  connectDB;
+
+
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected: ${conn.connection.name}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-    mongoose.connection.on('connected', ()=>{
-        console.log("Database Connected")
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/lumeo`)
-}
-
-export default  connectDB;
+export default connectDB;
