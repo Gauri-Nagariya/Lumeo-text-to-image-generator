efbf84dcd157b693f33cdceabe06b67becc7346d
Gauri Nagariya <gaurinagariya@gmail.com>
2025-09-30 14:42:43 +0530

    changed local to public url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'https://lumeo-text-to-image-generator.vercel.app/', // replace with your actual frontend URL
+  credentials: true,
+}));
 await connectDB()
 
 
