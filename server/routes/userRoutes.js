40c23159d45ce63def09ee4d35b9aec52240d9f5
Gauri Nagariya <gaurinagariya@gmail.com>
2025-09-30 14:29:51 +0530

    user routes changed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,10 @@ import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
+userRouter.get('/', (req, res) => {
+  res.send("User API is working!")
+})
+
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/credits', userAuth, userCredits)
