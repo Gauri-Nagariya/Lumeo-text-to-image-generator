e08b889f3dda394d00a909b12efb7fe3aeba8b71
Gauri Nagariya <gaurinagariya@gmail.com>
2025-10-14 13:34:44 +0530

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -15,10 +15,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database connected: ${conn.connection.name}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
