0ae50f19a784d5048a3dd2c49b7b344064701476
Gauri Nagariya <gaurinagariya@gmail.com>
2025-11-23 21:44:31 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 // import Testimonial from '../components/Testimonial'
-// import GenerateButton from '../components/GenerateButton'
+import GenerateButton from '../components/GenerateButton'
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       <Steps/>
       <Description/>
       {/* <Testimonial/> */}
-      {/* <GenerateButton/> */}
+     <GenerateButton/> 
     </div>
   )
 }
